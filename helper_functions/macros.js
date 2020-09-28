2e0e41b0d17912a44e7867c1d006d8071745b194
Gabriel Geisler Mesevage <gabriel.mesevage@gmail.com>
2020-09-28 13:09:54 +0300

    first commit

---
+++ helper_functions/macros.js
@@ -0,0 +1,8 @@
+remark.macros.scale = function(w) {
+  var url = this;
+  return '<img src="' + url + '" style="width: ' + w + '" />';
+};
+
+.large { font-size: 130% };
+
+.small { font-size: 70% };
